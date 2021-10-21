7dea700fe210604b9a24d5d71920c89bb8210997
Albert Neves <albert.neves.souza@gmail.com>
2021-10-21 09:39:34 -0300

    Criação de metodo de exclusão de Colaborador no Repositório

--- AugustosFashion/Repositorios/ColaboradorRepositorio.cs
+++ AugustosFashion/Repositorios/ColaboradorRepositorio.cs
@@ -16,10 +16,10 @@ namespace AugustosFashion.Repositorios
 {
     class ColaboradorRepositorio
     {
-        SqlConnection sqlCon = new SqlHelper().ObterConexao();
-
         public void CadastrarColaborador(ColaboradorModel colaborador, EnderecoModel endereco, List<TelefoneModel> telefones, ContaBancariaModel contaBancaria)
         {
+            SqlConnection sqlCon = new SqlHelper().ObterConexao();
+
             int insertedId = 0;
 
             var strSqlColaborador = "Insert into Colaboradores output inserted.IdColaborador " +
@@ -63,6 +63,48 @@ namespace AugustosFashion.Repositorios
             {
                 tran.Rollback();
                 throw new Exception(ex.Message);
+            }
+        }
+
+        public void ExcluirColaborador(int idColaborador)
+        {
+            SqlConnection sqlCon = new SqlHelper().ObterConexao();
+            int idUsuario = 0;
+
+            string strSqlRecuperaIdUsuario = @"select IdUsuario from Colaboradores where IdColaborador = @IdColaborador";
+            string strSqlExcluirColaborador = "delete from Colaboradores where IdColaborador = @IdColaborador";
+            string strSqlExcluirContaBancaria = ContaBancariaRepositorio.ObterStringExclusaoConta();
+            string strSqlExcluirEndereco = EnderecoRepositorio.ObterStringExcluisaoEndereco();
+            string strSqlExcluirTel = TelefoneRepositorio.ObterStringExclusaoTelefone();
+            string strSqlExcluirUsuario = UsuarioRepositorio.ObterStringExclusaoUsuario();
+
+            sqlCon.Open();
+
+            SqlTransaction tran = sqlCon.BeginTransaction();
+
+            try
+            {
+                //using (sqlCon)
+                //{
+                idUsuario = sqlCon.ExecuteScalar<int>(strSqlRecuperaIdUsuario, new { IdColaborador = idColaborador }, tran);
+
+                sqlCon.Execute(strSqlExcluirContaBancaria, new { IdColaborador = idColaborador }, tran);
+                sqlCon.Execute(strSqlExcluirColaborador, new { IdColaborador = idColaborador }, tran);
+                sqlCon.Execute(strSqlExcluirEndereco, new { IdUsuario = idUsuario }, tran);
+                sqlCon.Execute(strSqlExcluirTel, new { IdUsuario = idUsuario }, tran);
+                sqlCon.Execute(strSqlExcluirUsuario, new { IdUsuario = idUsuario }, tran);
+
+                tran.Commit();
+                //}
+            }
+            catch (Exception ex)
+            {
+                tran.Rollback();
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
             }
         }
     }
