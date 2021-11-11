e4c244a39a039591f5737b5864a70d9bbe7337d3
Albert Souza <albert.neves.souza@gmail.com>
2021-11-11 14:42:00 -0300

    Criação de Repositório de Pedido e método de insert

---
+++ AugustosFashion/Repositorios/PedidoRepositorio.cs
@@ -0,0 +1,39 @@
+﻿using AugustosFashion.Helpers;
+using AugustosFashionModels.Entidades.Pedidos;
+using Dapper;
+using System;
+using System.Data.SqlClient;
+
+namespace AugustosFashion.Repositorios
+{
+    internal class PedidoRepositorio
+    {
+        public static void CadastrarPedido(PedidoModel pedido)
+        {
+            var strSqlPedido = "Insert into Pedidos (IdCliente, IdColaborador, FormaPagamento, DataEmissao, TotalBruto, TotalDesconto, TotalLiquido) " +
+                "values (@IdCliente, @IdColaborador, @FormaPagamento, @DataEmissao, @TotalBruto, @TotalDesconto, @TotalLiquido)";
+
+            var strSqlPedidoProduto = @"insert into Pedido_Produto (IdPedido, IdProduto, Quantidade, Desconto, PrecoLiquido, Total)
+                values (@IdPedido, @IdProduto, @Quantidade, @Desconto, @PrecoLiquido, @Total) ";
+
+            try
+            {
+                using (SqlConnection sqlCon = SqlHelper.ObterConexao())
+                {
+                    sqlCon.Open();
+
+                    using(SqlTransaction transaction = sqlCon.BeginTransaction())
+                    {
+                        pedido.IdPedido = sqlCon.ExecuteScalar<int>(strSqlPedido, pedido, transaction);
+
+                        sqlCon.Execute(strSqlPedidoProduto, pedido.Produtos, transaction);
+                    }                   
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
